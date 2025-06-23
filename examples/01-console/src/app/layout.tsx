30447fce08cf7611abbefe345b4207181f8612fa
Jon Taylor <heyjtaylor@gmail.com>
2025-06-23 16:21:55 +0100

    fixed font rendering

--- examples/01-console/src/app/layout.tsx
+++ examples/01-console/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
+
 import "@pipecat-ai/voice-ui-kit/styles.css";
 
 const geistSans = Geist({
--- examples/01-console/src/app/page.tsx
+++ examples/01-console/src/app/page.tsx
@@ -5,9 +5,35 @@ import { ConsoleTemplate, ThemeProvider } from "@pipecat-ai/voice-ui-kit";
 export default function Home() {
   return (
     <ThemeProvider>
-      <ConsoleTemplate
-        onConnect={() => Promise.resolve(new Response("Connected"))}
-      />
+      <div className="w-full h-dvh bg-background">
+        <ConsoleTemplate
+          transportType="daily"
+          onConnect={async () => {
+            // Call the connect serverless function and get the room URL and token from Daily
+            const response = await fetch("/api/connect", {
+              method: "POST",
+              body: JSON.stringify({
+                createDailyRoom: true,
+              }),
+            });
+            if (!response.ok) {
+              throw new Error("Failed to connect to Pipecat");
+            }
+            const data = await response.json();
+            if (data.error) {
+              throw new Error(data.error);
+            }
+
+            return new Response(
+              JSON.stringify({
+                room_url: data.dailyRoom,
+                token: data.dailyToken,
+              }),
+              { status: 200 },
+            );
+          }}
+        />
+      </div>
     </ThemeProvider>
   );
 }
