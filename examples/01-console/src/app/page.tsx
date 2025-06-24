34b5bf1e334d6b09b48e16e82fdfe2333489936d
Jon Taylor <heyjtaylor@gmail.com>
2025-06-24 21:54:07 +0100

    removed dev work

--- examples/01-console/src/app/page.tsx
+++ examples/01-console/src/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { ConsoleTemplate, ThemeProvider } from "@pipecat-ai/voice-ui-kit";
-import { Plasma } from "@pipecat-ai/voice-ui-kit/webgl";
 
 export default function Home() {
   return (
     <ThemeProvider>
       <div className="w-full h-dvh bg-background">
-        <Plasma />
         <ConsoleTemplate
           transportType="daily"
           onConnect={async () => {
