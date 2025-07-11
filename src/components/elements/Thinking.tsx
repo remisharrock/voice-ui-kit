f55e82169c27c484a0a75d79ac8f45fff132cbe9
Christian Stuff <christian@daily.co>
2025-07-11 11:46:40 +0200

    add customization props to elements

--- src/components/elements/Thinking.tsx
+++ src/components/elements/Thinking.tsx
@@ -1,26 +1,36 @@
 import React, { useEffect, useState } from "react";
 
 interface ThinkingProps {
+  "aria-label"?: string;
   className?: string;
+  initialDots?: number;
+  interval?: number;
+  maxDots?: number;
 }
 
-export const Thinking: React.FC<ThinkingProps> = ({ className = "" }) => {
-  const [dots, setDots] = useState(1);
+export const Thinking: React.FC<ThinkingProps> = ({
+  "aria-label": ariaLabel = "Loading",
+  className = "",
+  initialDots = 1,
+  interval = 500,
+  maxDots = 3,
+}) => {
+  const [dots, setDots] = useState(initialDots);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setDots((prevDots) => (prevDots % 3) + 1);
-    }, 500);
+    const i = setInterval(() => {
+      setDots((prevDots) => (prevDots % maxDots) + 1);
+    }, interval);
 
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(i);
+  }, [interval, maxDots]);
 
   const renderDots = () => {
     return ".".repeat(dots);
   };
 
   return (
-    <span className={className} aria-label="Loading">
+    <span className={className} aria-label={ariaLabel}>
       {renderDots()}
     </span>
   );
