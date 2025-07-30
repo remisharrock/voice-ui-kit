bacf4b0868907d5c998aa374407cbe1e0323d4fd
Jon Taylor <heyjtaylor@gmail.com>
2025-07-30 15:42:06 +0100

    fixed resizable handle cursor

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import * as ResizablePrimitive from "react-resizable-panels";
-import { disableGlobalCursorStyles } from "react-resizable-panels";
+import {
+  disableGlobalCursorStyles,
+  usePanelGroupContext,
+} from "react-resizable-panels";
 
 import { EllipsisVerticalIcon } from "@/icons";
 import { cn } from "@/lib/utils";
@@ -36,12 +39,13 @@ function ResizableHandle({
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
   withHandle?: boolean;
 }) {
+  const { direction } = usePanelGroupContext();
   return (
     <ResizablePrimitive.PanelResizeHandle
       data-slot="resizable-handle"
       className={cn(
         "vkui:focus-visible:ring-ring vkui:relative vkui:flex vkui:w-px vkui:items-center vkui:justify-center vkui:after:absolute vkui:after:inset-y-0 vkui:after:left-1/2 vkui:after:w-1 vkui:after:-translate-x-1/2 vkui:focus-visible:ring-1 vkui:focus-visible:ring-offset-1 focus-visible:outline-hidden vkui:data-[panel-group-direction=vertical]:h-px vkui:data-[panel-group-direction=vertical]:w-full vkui:data-[panel-group-direction=vertical]:after:left-0 vkui:data-[panel-group-direction=vertical]:after:h-1 vkui:data-[panel-group-direction=vertical]:after:w-full vkui:data-[panel-group-direction=vertical]:after:-translate-y-1/2 vkui:data-[panel-group-direction=vertical]:after:translate-x-0 vkui:[&[data-panel-group-direction=vertical]>div]:rotate-90",
-        props["aria-orientation"] === "vertical"
+        direction === "vertical"
           ? "vkui:cursor-row-resize"
           : "vkui:cursor-col-resize",
         className,
@@ -49,14 +53,7 @@ function ResizableHandle({
       {...props}
     >
       {withHandle && (
-        <div
-          className={cn(
-            "vkui:border-transparent vkui:z-10 vkui:flex vkui:h-8 vkui:w-4 vkui:items-center vkui:justify-center vkui:rounded-xs vkui:border vkui:text-subtle vkui:hover:text-foreground vkui:focus:text-foreground",
-            {
-              "vkui:h-4 vkui:w-8 ": props["aria-orientation"] === "horizontal",
-            },
-          )}
-        >
+        <div className="vkui:border-transparent vkui:z-10 vkui:flex vkui:h-8 vkui:w-4 vkui:items-center vkui:justify-center vkui:rounded-xs vkui:border vkui:text-subtle vkui:hover:text-foreground vkui:focus:text-foreground">
           <EllipsisVerticalIcon size={16} />
         </div>
       )}
