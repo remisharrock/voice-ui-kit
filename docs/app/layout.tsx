d3634c37ef8c8f544ab1285947e740b6789626f5
Jon Taylor <heyjtaylor@gmail.com>
2025-08-04 19:31:37 +0300

    update docs with necessary components

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,16 +3,16 @@ import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { RootProvider } from "fumadocs-ui/provider";
-import { Inter } from "next/font/google";
+import { Geist } from "next/font/google";
 import type { ReactNode } from "react";
 
-const inter = Inter({
+const geist = Geist({
   subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={geist.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>
           <DocsLayout tree={source.pageTree} {...baseOptions}>
