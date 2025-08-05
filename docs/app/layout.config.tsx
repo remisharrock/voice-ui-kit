d9403d3f4aa81defc0ced3866a75082d38b3deda
Jon Taylor <heyjtaylor@gmail.com>
2025-08-05 18:20:59 +0300

    added doc meta content

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -5,14 +5,22 @@ export const baseOptions: BaseLayoutProps = {
     title: (
       <>
         <svg
-          width="24"
-          height="24"
+          width="22"
+          height="22"
+          viewBox="0 0 600 600"
+          fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
+          className="rounded-sm dark:invert"
         >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
+          <rect width="600" height="600" fill="black" />
+          <path d="M0 190H245V275H0V190Z" fill="white" />
+          <path d="M0 360H245V445H0V360Z" fill="white" />
+          <path
+            d="M498 220.5C498 262.197 464.197 296 422.5 296C380.803 296 347 262.197 347 220.5C347 178.803 380.803 145 422.5 145C464.197 145 498 178.803 498 220.5Z"
+            fill="white"
+          />
         </svg>
-        Voice UI Kit
+        <span className="font-semibold">Voice UI Kit</span>
       </>
     ),
   },
--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,6 +3,7 @@ import { baseOptions } from "@/app/layout.config";
 import { source } from "@/lib/source";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import { RootProvider } from "fumadocs-ui/provider";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { ReactNode } from "react";
 
@@ -15,6 +16,17 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://voiceuikit.pipecat.ai"),
+  title: {
+    default:
+      "Voice UI Kit - Components, hooks and template apps for building voice AI applications quickly.",
+    template: "%s - Voice UI Kit",
+  },
+  description:
+    "Components, hooks and template apps for building React voice AI applications quickly. Designed to support and accelerate Pipecat AI development.",
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
